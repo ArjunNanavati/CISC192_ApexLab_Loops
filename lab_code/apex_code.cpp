7081b240214b2a8aedb661bfe365d291a8545a19
ArjunNanavati <tootsie.nanavati@gmail.com>
2025-10-18 18:06:22 -0700

    apex_code.cpp

--- lab_code/apex_code.cpp
+++ lab_code/apex_code.cpp
@@ -1,5 +1,6 @@
 //
 //
+#include <iomanip>  
 #include <iostream>
 #include "apex_code.h"
 
@@ -11,12 +12,11 @@ void email_concat(stringstream& cin, stringstream& cout) {
     while (cin >> addr) {
         cout << addr << ":";
     }
-    cout << endl;
 }
 
 void donation_total(stringstream &cin, stringstream &cout) {
     double amt;
-    double total = 0.0;            // keep reading until a negative number appears
+    double total = 0.0;            // keep reading until negative number appears
     
     while (cin >> amt) {
         if (amt < 0) {
@@ -29,30 +29,28 @@ void donation_total(stringstream &cin, stringstream &cout) {
     }
 
     cout << fixed << setprecision(2);
-    cout << "Total donations made $" << total << endl;
+    cout << "Total donations made $" << total;
 }
 
 void average_books_checked_out(std::stringstream &cin, std::stringstream &cout) {
-    int monthCount = 12;
+ const int monthCount = 12;
     long sum = 0;
     bool Over1268 = true;
 
     for (int i = 0; i < monthCount; ++i) {
-        long x;
-        cin >> x;
-        sum = sum + x;
-        if (x <= 1268) {                    // "more than 1268" = greater
-            Over1268 = false;
-        }
+        long x; cin >> x;
+        sum += x;
+        if (x <= 1268) Over1268 = false; // more than 1268
     }
 
-    long avg = sum / monthCount;    // integer average like the examples
+    long avg = sum / monthCount;
+    cout << "Average monthly circulation: " << avg << " books";
 
-    cout << "Average monthly circulation: " << avg << " books" << endl;
     if (Over1268 && avg > 2500) {
-        cout << "Extend hours!!" << endl;
+        cout << '\n' << "Extend hours!!";   // no newline after this
+    } else {
+        cout << '\n';                      
     }
-    
 }
 
 void rectangles(std::stringstream &cin, std::stringstream &cout) {
@@ -62,10 +60,9 @@ void rectangles(std::stringstream &cin, std::stringstream &cout) {
 
     for (int r = 0; r < height; ++r) {
         for (int c = 0; c < width; ++c) {
-            cout << ch;
-            if (c < width - 1) cout << " ";   // same spacing: X X X
+            cout << ch << ' ';           // space after each character 
         }
-        cout << endl;
+        cout << '\n';
     }
 }
 
